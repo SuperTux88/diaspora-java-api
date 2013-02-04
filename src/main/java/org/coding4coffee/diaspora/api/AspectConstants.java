029829e243d451815770dd6a16d90492b6a5d240
Benjamin Neff <benjamin@coding4coffee.ch>
2013-02-04 02:45:35 +0100

    added aspect constants

---
+++ src/main/java/org/coding4coffee/diaspora/api/AspectConstants.java
@@ -0,0 +1,6 @@
+package org.coding4coffee.diaspora.api;
+
+public interface AspectConstants {
+	String PUBLIC = "public";
+	String ALL_ASPECTS = "all_aspects";
+}
--- src/test/java/org/coding4coffee/diaspora/api/sandbox/DiasporaClientSandbox.java
+++ src/test/java/org/coding4coffee/diaspora/api/sandbox/DiasporaClientSandbox.java
@@ -2,6 +2,7 @@ package org.coding4coffee.diaspora.api.sandbox;
 
 import java.util.Arrays;
 
+import org.coding4coffee.diaspora.api.AspectConstants;
 import org.coding4coffee.diaspora.api.ClientFactory;
 import org.coding4coffee.diaspora.api.DiasporaClient;
 
@@ -16,8 +17,8 @@ public class DiasporaClientSandbox {
 		System.out.println("login successful: " + client.login("api", "apitest"));
 
 		System.out.println("aspects: " + client.getAspects());
-		System.out.println("post id: " + client.post("test 123", "public"));
-		System.out.println("post id: " + client.post("test 1234", "all_aspects"));
+		System.out.println("post id: " + client.post("test 123", AspectConstants.PUBLIC));
+		System.out.println("post id: " + client.post("test 1234", AspectConstants.ALL_ASPECTS));
 		System.out.println("post id: " + client.post("test 12345", Arrays.asList(new String[] { "21", "22" })));
 	}
 }
